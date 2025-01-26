78f14d263f87552b596847e07c8865b3f39c2c38
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-01-26 09:33:18 +0700

    feat: better container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,16 @@ export default {
   darkMode: ["class"],
   content: ["./src/**/*.tsx"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
