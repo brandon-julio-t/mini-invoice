070ac442b7f92dc1601912ef9d227d4e42e2f240
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-05-01 19:32:50 +0700

    chore: remove unused code + fix spacing

--- src/app/_components/backup-restore-section.tsx
+++ src/app/_components/backup-restore-section.tsx
@@ -1,14 +1,5 @@
 import { Button } from "@/components/ui/button";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import {
   Drawer,
   DrawerContent,
   DrawerDescription,
@@ -113,7 +104,7 @@ export const BackupRestoreSection = () => {
               Restore
             </Button>
           </DrawerTrigger>
-          <DrawerContent className="px-4 pb-4">
+          <DrawerContent className="pb-4">
             <DrawerHeader>
               <DrawerTitle>Restore</DrawerTitle>
               <DrawerDescription>
@@ -138,39 +129,5 @@ export const BackupRestoreSection = () => {
         </Drawer>
       </DrawerContent>
     </Drawer>
-  );
-  return (
-    <div className="flex flex-col gap-2">
-      <Button variant="outline" onClick={onBackup}>
-        <DatabaseBackupIcon />
-        Backup
-      </Button>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline">
-            <ArchiveRestoreIcon />
-            Restore
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="max-h-screen overflow-y-auto">
-          <DialogHeader>
-            <DialogTitle>Restore</DialogTitle>
-            <DialogDescription>
-              Paste the copied text from backup button
-            </DialogDescription>
-          </DialogHeader>
-
-          <Textarea
-            value={restoreText}
-            onChange={(e) => setRestoreText(e.target.value)}
-            className="max-h-64 resize-none"
-          />
-
-          <DialogFooter>
-            <Button onClick={onRestore}>Restore</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
   );
 };
