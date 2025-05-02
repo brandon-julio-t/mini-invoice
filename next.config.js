10edd2b46e31dae772ca82c114240c8a9267faa6
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-05-02 20:29:41 +0700

    feat: security headers by next.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,46 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  /** @docs https://nextjs.org/docs/app/guides/progressive-web-apps#8-securing-your-application */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/javascript; charset=utf-8",
+          },
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self'",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default config;
